241a0b072c018752d9dd13024372a37006a2a1c4
Odiadestiny <odiadestiny56@gmail.com>
2025-04-12 07:28:28 -0400

    Authorization Implemented

--- obj/Debug/net9.0/ERMS.AssemblyInfo.cs
+++ obj/Debug/net9.0/ERMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac7e30fddcfe174397dc22df06fae87823b3d22d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d340cbb72097ab063aa606d39ce53336078bdc2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
